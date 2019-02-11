77d9b57aabc84178de0b2b47e08382ca1b1c3152
William Robert Simpson <will.simpson85@gmail.com>
2019-02-11 17:39:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -73,7 +73,7 @@ puts "#{colour} with #{names}"
     new_bird_hash.keys.each do |s|
       if x == s
       puts "#{colour} and #{x}"
-      new_bird_hash[x][:color] << color
+      new_bird_hash[x][:color] << colour
       end
     end
 
